d5f65ec616a2eabb1b42bcb8eb192b7a2a65f3b9
Simon Feiden <simon.feiden@rwth-aachen.de>
2018-10-18 11:26:56 +0200

    Simple event queue

---
+++ server/eventing/event_queue.py
@@ -0,0 +1,67 @@
+import asyncio as aio
+import datetime
+
+from collections import defaultdict
+
+
+class Event(object):
+  def __init__(self):
+    self.occured = datetime.now()
+    self.event_queue = None
+
+
+class HandlerFailedEvent(object):
+  def __init__(self, handler):
+    super().__init__()
+    self.failed_handler = failed_handler
+
+
+class EventQueue(object):
+  def __init__(self, loop):
+    self.queue = aio.Queue(loop=loop)
+    self.handlers = defaultdict(list)
+    self.any_handlers = []
+
+  def register(self, event_class, handler):
+    self.handlers[event_class].append(handler)
+
+  def register_for_all(self, handler):
+    self.any_handlers.append(handler)
+
+  def register_class(self, clss):
+    try:
+      # can also be a list of events
+      for event_class in clss.events:
+        self.register(event_class, clss)
+    except TypeError:
+      self.register(clss.events, clss)
+
+  async def publish(self, event):
+    await self.queue.put(event)
+
+  async def run(self):
+    while True:
+      print('await queue')
+      event = await self.queue.get()
+      print('got event')
+      event.event_queue = self
+
+      event_class = type(event)
+      for handler in self.handlers[event_class]:
+        await self.fire_event(event, handler)
+
+      for handler in self.any_handlers:
+        await self.fire_event(event, handler)
+
+      self.queue.task_done()
+
+  async def fire_event(self, event, handler):
+      event_class = type(event)
+      try:
+        result = handler(event)
+
+        # event handler may be async
+        if aio.iscoroutine(result):
+          await result
+      except:
+        await self.publish(HandlerFailedEvent(handler))
