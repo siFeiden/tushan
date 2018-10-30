f0fcbfa88983819296f62f7f98ad4aefd51ee9d8
Simon Feiden <simon.feiden@rwth-aachen.de>
2018-10-30 18:06:17 +0100

    Test generation of all valid placements on board

---
+++ server/test/arena/test_board_list_valid_placements.py
@@ -0,0 +1,31 @@
+import unittest
+
+from arena.piece import Orientation, Piece
+from arena.game import Board
+
+
+class ValidPlacementsTest(unittest.TestCase):
+  def setUp(self):
+    self.piece1 = Piece(2, 2, [2, 5])
+    self.piece2 = Piece(1, 3, [0, 1, 4])
+
+  def test_list_valid_placements(self):
+    board = Board(8, self.piece1)
+
+    placements = list(board.valid_placements(self.piece2))
+
+    for placed_piece in placements:
+      self.assertIsNone(board.validate_placement(placed_piece))
+
+    expected_placements = [
+      (5, 4, Orientation.East),
+      (6, 4, Orientation.North),
+      (8, 3, Orientation.West),
+      (3, 5, Orientation.South),
+      (3, 6, Orientation.East),
+      (4, 8, Orientation.North)
+    ]
+
+    for expected_placement in expected_placements:
+      ok = any(expected_placement == (p.x, p.y, p.orientation) for p in placements)
+      self.assertTrue(ok)
