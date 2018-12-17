354c4f6c46b1a3be6b242bc97fa8c9b07f9b14d5
Simon Feiden <simon.feiden@rwth-aachen.de>
2018-12-17 17:29:38 +0100

    Factor out helper classes of lobby test

--- server/test/arena/lobby/test_lobby.py
+++ server/test/arena/lobby/test_lobby.py
@@ -1,79 +1,10 @@
-import json
+from asynctest import TestCase
 
-from asynctest import CoroutineMock, MagicMock, TestCase
-
-from main import BootstrapEvent
-from arena.lobby import Lobby
-from arena.logic.piece import Piece
-from arena.events import *
-from eventing.event_queue import EventQueue, HandlerFailedEvent
-from net.server import ClientConnectedEvent
-
-
-class GameSpec(object):
-  def __init__(self, players, pieces, positions):
-    self.players = players
-    self.pieces = pieces
-    self.positions = positions
-
-    assert len(self.players) == 2
-
-  async def play_on(self, event_queue):
-    for player in self.players:
-      event = ClientConnectedEvent(player, None, None)
-      await event_queue.publish(event)
-
-    await event_queue.run_until_complete()
-
-    i = 0
-    for piece, (x, y, orientation) in zip(self.pieces, self.positions):
-      player = self.players[i]
-      event = PlayerMoveEvent(player, x, y, orientation)
-      await event_queue.publish(event)
-      await event_queue.run_until_complete()
-      i = 1 - i
-
-  @classmethod
-  def from_file(clss, path):
-    with open(path) as f:
-      game = json.load(f)
-
-      players = list(game['objectives'])
-      pieces = []
-      positions = []
-      for p in game['stones']:
-        pieces.append(Piece(p['width'], p['height'], p['connectors']))
-        pos = p['position']
-        positions.append((pos['x'], pos['y'], pos['orientation']))
-
-      return clss(players, pieces, positions)
-
-
-class GamePlan(object):
-  def __init__(self, gamespec):
-    self.gamespec = gamespec
-    self.event_queue = EventQueue(debug=True)
-
-    random = MagicMock()
-    random.sample.side_effect = lambda l, n: l[:n]
-    self.lobby = Lobby(random)
-
-  async def run(self):
-    self.event_queue.register(BootstrapEvent, self.lobby)
-    await self.event_queue.publish(BootstrapEvent())
-    await self.event_queue.run_until_complete()
-    await self.gamespec.play_on(self.event_queue)
+from test.games import GamePlan, GameSpec
 
 
 class LobbyTest(TestCase):
-  def handler_failed(self, event):
-    print('ERROR:', event.exception)
-
   async def test_spec_game(self):
     gamespec = GameSpec.from_file('../protocol/examples/populated-field.json')
     gameplan = GamePlan(gamespec)
-    gameplan.event_queue.register(HandlerFailedEvent, lambda e: print(e.exception))
-    await gameplan.run()
-
-    import arena.serializers as s
-    print(s.GameSerializer.serialize(gameplan.lobby.game))
+    await gameplan.execute()
---
+++ server/test/games.py
@@ -0,0 +1,71 @@
+import json
+
+from asynctest import MagicMock
+
+from main import BootstrapEvent
+from arena.lobby import Lobby
+from arena.logic.game import Board, Game, Player
+from arena.logic.piece import Orientation, Piece
+from arena.events import *
+from eventing.event_queue import EventQueue, HandlerFailedEvent
+from net.server import ClientConnectedEvent
+
+
+class GameSpec(object):
+  def __init__(self, players, pieces, positions):
+    self.players = players
+    self.pieces = pieces
+    self.positions = positions
+
+    assert len(self.players) == 2
+
+  @classmethod
+  def from_file(clss, path):
+    with open(path) as f:
+      game = json.load(f)
+
+      players = list(game['objectives'])
+      pieces = []
+      positions = []
+      for p in game['stones']:
+        pieces.append(Piece(p['width'], p['height'], p['connectors']))
+        pos = p['position']
+        x = pos['x']
+        y = pos['y']
+        orientation = Orientation(pos['orientation'])
+        positions.append((x, y, orientation))
+
+      return clss(players, pieces, positions)
+
+
+class GamePlan(object):
+  def __init__(self, gamespec):
+    self.gamespec = gamespec
+    self.event_queue = EventQueue()
+
+    random = MagicMock()
+    random.sample.side_effect = lambda l, n: l[:n]
+    random.shuffle.side_effect = self.set_lobby_pieces
+    self.lobby = Lobby(random)
+
+  def set_lobby_pieces(self, piece_list):
+    piece_list.clear()
+    piece_list.extend(self.gamespec.pieces)
+
+  async def execute(self):
+    self.event_queue.register(BootstrapEvent, self.lobby)
+    await self.event_queue.publish(BootstrapEvent())
+    await self.event_queue.run_until_complete()
+
+    for player in self.gamespec.players:
+      event = ClientConnectedEvent(player, None, None)
+      await self.event_queue.publish(event)
+      await self.event_queue.run_until_complete()
+
+    i = 0
+    for piece, (x, y, orientation) in zip(self.gamespec.pieces, self.gamespec.positions):
+      player = self.gamespec.players[i]
+      event = PlayerMoveEvent(player, x, y, orientation)
+      await self.event_queue.publish(event)
+      await self.event_queue.run_until_complete()
+      i = 1 - i
