d60e4f86e86b71cdd39c389542b45d710417227a
Simon Feiden <simon.feiden@rwth-aachen.de>
2018-12-18 21:19:54 +0100

    Use EventQueueTestMixin in lobby test

--- server/test/arena/lobby/test_lobby.py
+++ server/test/arena/lobby/test_lobby.py
@@ -1,10 +1,18 @@
 from asynctest import TestCase
 
+from arena.events import GameIsOverEvent, MoveAcceptedEvent
+from test.event_queue import EventQueueTestMixin
 from test.games import GamePlan, GameSpec
 
 
-class LobbyTest(TestCase):
+class LobbyTest(TestCase, EventQueueTestMixin):
   async def test_spec_game(self):
     gamespec = GameSpec.from_file('../protocol/examples/populated-field.json')
     gameplan = GamePlan(gamespec)
-    await gameplan.execute()
+    event_queue = self.event_queue_mock()
+    await gameplan.execute_on(event_queue)
+
+    moves = len(gamespec.pieces) - 1
+    # last successful moves generates GameIsOverEvent
+    events = [*[MoveAcceptedEvent] * moves, GameIsOverEvent]
+    self.assertEventsFuzzy(event_queue, events)
