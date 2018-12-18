0f624831c83ee4b26c6ccf5bbb42d1f756c6897b
Simon Feiden <simon.feiden@rwth-aachen.de>
2018-12-18 21:17:01 +0100

    Improve EventQueueTestMixin
    Add EventQueue subclass that logs all published events.
    This allows much better assert* methods in the mixin to
    assert which events were published in what order.

--- server/test/event_queue.py
+++ server/test/event_queue.py
@@ -1,19 +1,53 @@
-from asynctest.mock import CoroutineMock, MagicMock
 from eventing.event_queue import EventQueue
 
 
 class EventQueueTestMixin(object):
   def event_queue_mock(self):
-    return CoroutineMock(spec=EventQueue)
+    return TestingEventQueue()
 
   def assertEventSequence(self, event_queue, events):
-    publish_calls = event_queue.publish.call_args_list
-
-    call_count = event_queue.publish.call_count
+    actual_events = event_queue.all_events
+    call_count = len(actual_events)
     if len(events) != call_count:
       raise AssertionError(f'Expected {len(events)} events, got {call_count}')
 
-    for (args, kwargs), event in zip(publish_calls, events):
-      if not isinstance(args[0], event):
-        arg_type_name = type(args[0]).__name__
-        raise AssertionError(f'{arg_type_name} is not a {event.__name__} event')
+    for published, expected in zip(actual_events, events):
+      if not isinstance(published, expected):
+        arg_type = type(published).__name__
+        raise AssertionError(f'{arg_type} is not a {expected.__name__} event')
+
+  def assertEventPublishedNTimes(self, event_queue, n, event):
+    i = 0
+    for published in event_queue.all_events:
+      if isinstance(published, event):
+        i += 1
+
+    if i != n:
+      raise AssertionError(f'{event.__name__} expected {n} times but found {i} times')
+
+  def assertEventsFuzzy(self, event_queue, events):
+    events = list(events) # copy to not modify passed list
+    for published in event_queue.all_events:
+      if isinstance(published, events[0]):
+        events.pop(0)
+        if len(events) == 0:
+          return # found all events
+
+    remaining = len(events)
+    if remaining > 0:
+      names = ', '.join(e.__name__ for e in events)
+      raise AssertionError(f'Expected to match {remaining} more events: [{names}]')
+
+
+class TestingEventQueue(EventQueue):
+  """Extends a normal EventQueue and keeps all
+  events that are published
+  """
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.all_events = []
+
+  async def publish(self, event):
+    self.all_events.append(event)
+    return await super().publish(event)
